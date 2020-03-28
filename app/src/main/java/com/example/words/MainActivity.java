735d58f5785bf605fc5867596041d0aea5f9ddaa
Alan <452210222@qq.com>
2020-03-28 18:42:41 +0100

    Words step 1: migration the mainActivity to the wordsFragment.

---
+++ app/src/main/java/com/example/words/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.words;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/example/words/WordsFragement.java
@@ -0,0 +1,64 @@
+package com.example.words;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.SavedStateViewModelFactory;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WordsFragement extends Fragment {
+
+    private WordViewModel wordViewModel;
+    private RecyclerView recyclerView;
+    private MyAdapter myAdapter1,myAdapter2;
+
+    public WordsFragement() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_words_fragement, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        wordViewModel = new ViewModelProvider(requireActivity(),new SavedStateViewModelFactory(requireActivity().getApplication(),this)).get(WordViewModel.class);
+        recyclerView = requireActivity().findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+        myAdapter1 = new MyAdapter(false,wordViewModel);
+        myAdapter2 = new MyAdapter(true,wordViewModel);
+        recyclerView.setAdapter(myAdapter1); // 默认是适配器1
+        wordViewModel.getAllWordsLive().observe(requireActivity(), new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                int temp = myAdapter1.getItemCount();    //获取适配器有多少个子项
+                myAdapter1.setAllWords(words);
+                myAdapter2.setAllWords(words);
+                if (temp != words.size()) {
+                    myAdapter1.notifyDataSetChanged();
+                    myAdapter2.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+}
